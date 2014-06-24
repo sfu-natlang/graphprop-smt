5e7b04dc4e8f407efacfbe897ff8f411b9ced676
ramtinms <ramtinms@yahoo.com>
2014-06-24 00:13:07 -0700

    add labels tested

--- ppdb/add_label_to_graph.py
+++ ppdb/add_label_to_graph.py
@@ -13,13 +13,13 @@ class Labeler:
     id_to_label = {}
 
     label_counter = 0
-    max_number_of_labels
+    max_number_of_labels = 20
     processed_nodes = []
 
     def __init__(self, extract_path, graph_file,  id_to_phrase_file, max_number_of_labels=20):
         self.extract_path = extract_path
         self.graph_file = graph_file
-        self.extractor = cdec.sa.GrammarExtractor(extract_path+'\extract.ini')
+        self.extractor = cdec.sa.GrammarExtractor(extract_path+'/extract.ini')
         with open(id_to_phrase_file, 'rb') as input:
             self.id_to_phrase = pickle.load(input)
         self.max_number_of_labels = max_number_of_labels
@@ -27,9 +27,9 @@ class Labeler:
     # this will return a list of labels with probability
     def get_labels(self, phrase):
         result_list = []
-        grammars = extractor.grammar(phrase)        
+        grammars = self.extractor.grammar(phrase)        
         for rule in grammars:
-            parts = rule.split(" ||| ")
+            parts = str(rule).split(" ||| ")
             rule = parts[0:3]
             srule = " ||| ".join(rule)
             #print rule
@@ -49,35 +49,35 @@ class Labeler:
                 label_id = self.label_to_id[changed_rule]
                 
             else:
-                label_id = "RULEN"+str(label_counter)
-                #print "here" + "RULEN"+str(Lable_number)
-                id_to_label[label_id] = changed_rule
-                label_to_id[changed_rule] = label_id
-                label_counter +=1
+                label_id = "RULEN"+str(self.label_counter)
+                #print "here" + "RULEN"+str(self.label_number)
+                self.id_to_label[label_id] = changed_rule
+                self.label_to_id[changed_rule] = label_id
+                self.label_counter +=1
             result_list.append((label_id,prob))
                 ### sorting result list based on probability (for further cutting)
         result_list = sorted(result_list,key=lambda x: x[1], reverse = True)
         
         # cutting top labels
-        if len(result_list) > max_number_of_labels:
-            result_list =  result_list[:max_number_of_labels]
+        if len(result_list) > self.max_number_of_labels:
+            result_list =  result_list[0:self.max_number_of_labels]
 
         return result_list
 
     def add_labels_to_graph(self):
-        with open(self.graph_file,"w") as graph:
+        with open(self.graph_file,"r") as graph:
             with open("seeds","w") as seeds_file:
                 for line in graph:
                     for i in [0,1]:
                         node = line.strip().split()[i]
-                        if not node in processed_nodes:
-                            phrase = id_to_phrase(node)
-                            labels = get_labels(phrase)
+                        if not node in self.processed_nodes:
+                            phrase = self.id_to_phrase[node]
+                            labels = self.get_labels(phrase)
                             if labels:
                                 for (label,prob) in labels:
                                 # write to seed file
                                     seeds_file.write(node+"\t"+label+"\t"+prob+"\n")
-                            processed_nodes.add(node)
+                            self.processed_nodes.append(node)
 
     def save_to_file(self):
         with open('id_to_label.pkl', 'wb') as output:
@@ -87,5 +87,6 @@ class Labeler:
 # TODO main function
 
 if __name__=="__main__":
-    my_labeler = Labeler("extract", "basic_graph",  "id_to_phrase.pkl")
+    my_labeler = Labeler("extract", "basic_graph",  "unit_test_ppdb_processorid_to_phrase.pkl",20)
     my_labeler.add_labels_to_graph()
+    my_labeler.save_to_file()
