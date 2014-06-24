f0e4c356f0627491e8fbc07bae71f4dcf4b740e1
ramtinms <ramtinms@yahoo.com>
2014-06-23 20:42:33 -0700

    Processor updated

--- ppdb/ppdb_processor.py
+++ ppdb/ppdb_processor.py
@@ -14,46 +14,52 @@ class Processor:
     id_to_phrase = {}
     phrase_to_id_file = "phrase_to_id.pkl"
     id_to_phrase_file = "id_to_phrase.pkl"
-    phase_to_id_helper_file = "phrase_to_id"
+    phrase_to_id_helper_file = "phrase_to_id"
 
+    graph_output_file="basic_graph"
+    
     def __init__(self, ppdb_size="l", ppdb_type="lexical", graph_output_file="basic_graph", \
                  phrase_to_id_file="phrase_to_id.pkl", id_to_phrase_file="id_to_phrase.pkl", phase_to_id_helper_file="phrase_to_id"):
         self.para_input = "ppdb-1.0-"+ppdb_size+"-"+ppdb_type
         self.phrase_to_id_file = phrase_to_id_file
         self.id_to_phrase_file = id_to_phrase_file
-        self.phase_to_id_helper_file = phase_to_id_helper_file
+        self.phrase_to_id_helper_file = phase_to_id_helper_file
 
     def process(self):
         i = 0
-        with open(para_input) as myinput:
-            with open(phrase_to_id_helper_file,"w") as file_phrase_to_id:
-                with open(graph_output_file,"w") as file_basic_graph:
+        with open(self.para_input) as myinput:
+            with open(self.phrase_to_id_helper_file,"w") as file_phrase_to_id:
+                with open(self.graph_output_file,"w") as file_basic_graph:
                     for line in myinput:
                         list = line.split(" ||| ")
                         source=list[1]
                         target=list[2]
                         features=list[3]
-                        if not source in phrase_to_id:
-                            phrase_to_id[source] = "N"+str(i)
-                            id_to_phrase["N"+str(i)] = source
-                            file_phrase_to_id.write("N"+str(i)+"\t"+source)
+                        if not source in self.phrase_to_id:
+                            self.phrase_to_id[source] = "N"+str(i)
+                            self.id_to_phrase["N"+str(i)] = source
+                            file_phrase_to_id.write("N"+str(i)+"\t"+source+"\n")
                             i += 1
-                        if not target in phrase_to_id:
-                            phrase_to_id[target] = "N"+str(i)
-                            id_to_phrase["N"+str(i)] = target
-                            file_phrase_to_id.write("N"+str(i)+"\t"+target)
+                        if not target in self.phrase_to_id:
+                            self.phrase_to_id[target] = "N"+str(i)
+                            self.id_to_phrase["N"+str(i)] = target
+                            file_phrase_to_id.write("N"+str(i)+"\t"+target+"\n")
                             i += 1
                         
                         indx = features.find("Lex(e|f)=")
                         #e_given_f = features[indx+9:indx+17]
                         e_given_f = str(float(features[indx+9:indx+12])/100)
-                        file_basic_graph.write(phrase_to_id[source] + "\t" + phrase_to_id[target] + "\t" + e_given_f + "\n")
+                        file_basic_graph.write(self.phrase_to_id[source] + "\t" + self.phrase_to_id[target] + "\t" + e_given_f + "\n")
 
         print "saving part"
-        with open(phrase_to_id_file, 'wb') as output:
-            pickle.dump(phrase_to_id, output, pickle.HIGHEST_PROTOCOL)
+        with open(self.phrase_to_id_file, 'wb') as output:
+            pickle.dump(self.phrase_to_id, output, pickle.HIGHEST_PROTOCOL)
 
-        with open(id_to_phrase_file, 'wb') as output:
-            pickle.dump(id_to_phrase, output, pickle.HIGHEST_PROTOCOL)
+        with open(self.id_to_phrase_file, 'wb') as output:
+            pickle.dump(self.id_to_phrase, output, pickle.HIGHEST_PROTOCOL)
 
-                                                                                 
+if __name__=="__main__":
+   unique_name = "unit_test_ppdb_processor"
+   my_processor = Processor(ppdb_size="l", ppdb_type="lexical", graph_output_file=unique_name+"basic_graph", \
+                                 phrase_to_id_file=unique_name+"phrase_to_id.pkl", id_to_phrase_file=unique_name+"id_to_phrase.pkl", phase_to_id_helper_file=unique_name+"phrase_to_id") 
+   my_processor.process()                                                                              
