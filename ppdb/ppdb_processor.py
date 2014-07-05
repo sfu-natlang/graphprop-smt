d260d2c7c70d2ccbbd8d2f80a52d3771de580461
ramtinms <ramtinms@yahoo.com>
2014-07-04 18:26:09 -0700

    PPDB statistics reports

--- ppdb/ppdb_processor.py
+++ ppdb/ppdb_processor.py
@@ -16,6 +16,11 @@ class Processor:
     id_to_phrase_file = "id_to_phrase.pkl"
     phrase_to_id_helper_file = "phrase_to_id"
 
+    # TODO if reports required , for speed. right now , we are computing reports always.
+    report = True
+    node_to_node_connections = {}
+  
+
     graph_output_file="basic_graph"
     
     def __init__(self, ppdb_size="l", ppdb_type="lexical", graph_output_file="basic_graph", \
@@ -27,6 +32,7 @@ class Processor:
         self.graph_output_file = graph_output_file
     def process(self):
         i = 0
+        j = 0
         print "reading "+self.para_input
         with open(self.para_input) as myinput:
             with open(self.phrase_to_id_helper_file,"w") as file_phrase_to_id:
@@ -40,18 +46,40 @@ class Processor:
                             self.phrase_to_id[source] = "N"+str(i)
                             self.id_to_phrase["N"+str(i)] = source
                             file_phrase_to_id.write("N"+str(i)+"\t"+source+"\n")
+                            if self.report == True:
+                                self.node_to_node_connections["N"+str(i)]=[]
                             i += 1
                         if not target in self.phrase_to_id:
                             self.phrase_to_id[target] = "N"+str(i)
                             self.id_to_phrase["N"+str(i)] = target
                             file_phrase_to_id.write("N"+str(i)+"\t"+target+"\n")
+                            if self.report == True:
+                                self.node_to_node_connections["N"+str(i)]=[]
                             i += 1
-                        
                         indx = features.find("Lex(e|f)=")
                         #e_given_f = features[indx+9:indx+17]
                         e_given_f = str(float(features[indx+9:indx+12])/100)
+                        j += 1
+                        if self.report == True:
+                            #print self.phrase_to_id[source] + "\t" + self.phrase_to_id[target]
+                            self.node_to_node_connections[self.phrase_to_id[source]].append(self.phrase_to_id[target])
                         file_basic_graph.write(self.phrase_to_id[source] + "\t" + self.phrase_to_id[target] + "\t" + e_given_f + "\n")
-        print "Total number of nodes: "+str(i)
+
+        if self.report == True:
+            max_len_nodes = [k for k in self.node_to_node_connections.keys() if len(self.node_to_node_connections.get(k))==max([len(n) for n in self.node_to_node_connections.values()])] 
+            len_sum = float(sum([len(n) for n in self.node_to_node_connections.values()]))
+            average_len = len_sum / float(len(self.node_to_node_connections.keys()))
+            print len_sum
+            #TODO number of nodes with one lables
+
+            print 
+            print "------------------------reports----------------------------------"
+            print "Total number of nodes: "+str(i)
+            print "Total number of connections: "+str(j) 
+            print "Maximum Neighbours of a node: "+str(len(self.node_to_node_connections[max_len_nodes[0]]))
+            print "Average Neighbours of a node: "+str(average_len)
+            print "-----------------------------------------------------------------"
+
         print "Saving phrases to files"
         with open(self.phrase_to_id_file, 'wb') as output:
             pickle.dump(self.phrase_to_id, output, pickle.HIGHEST_PROTOCOL)
