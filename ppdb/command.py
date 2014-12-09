26a1634be2f48bdadfaa727854a1d1945cda2a4d
ramtinms <ramtinms@yahoo.com>
2014-12-09 13:35:10 -0800

    command.py now supports languages

--- ppdb/command.py
+++ ppdb/command.py
@@ -5,47 +5,55 @@ from ppdb_processor import Processor
 import sys
 
 ppdb_size = "l" # xxl xl l 
-ppdb_type = "lexical"
+ppdb_type = "lexical"  # lexical , phrasal
 domain = "europarl"
+task = "en-es"
 max_number_of_labels = 20
 unique_name = domain+"_"+ppdb_size+"_"+ppdb_type+"_"
 knn=20
 graph_type = "bipartite"
 
+label_mode = 1
 if __name__ == "__main__":
-    if len(sys.argv) > 3:
+    if len(sys.argv) > 4:
         ppdb_size = sys.argv[1]   # size
         ppdb_type = sys.argv[2]   # type
-        domain = sys.argv[3]      # domain
+        lang = sys.argv[3]
+        domain = sys.argv[4]      # domain
         
-        unique_name = domain+"_"+ppdb_size+"_"+ppdb_type+"_"
+        unique_name = "experiments/"+domain+"_"+task+"_"+ppdb_size+"_"+ppdb_type+"_"
 
-#        my_processor = Processor(ppdb_size=ppdb_size, 
-#                                 ppdb_type=ppdb_type,
-#                                 graph_output_file=unique_name+"basic_graph", 
-#                                 phrase_to_id_file=unique_name+"phrase_to_id.pkl",
-#                                 id_to_phrase_file=unique_name+"id_to_phrase.pkl",
-#                                 phase_to_id_helper_file=unique_name+"phrase_to_id") 
+        my_processor = Processor(ppdb_size=ppdb_size, 
+                                 ppdb_type=ppdb_type,
+                                 ppdb_lang=lang,
+                                 graph_output_file=unique_name+"basic_graph", 
+                                 phrase_to_id_file=unique_name+"phrase_to_id.pkl",
+                                 id_to_phrase_file=unique_name+"id_to_phrase.pkl",
+                                 phase_to_id_helper_file=unique_name+"phrase_to_id") 
 
         print "starting processing "+unique_name
-#        my_processor.process()
+        my_processor.process()
         print "end of proessing, Graph file is ready"
-     
+
         print "start transfering labels to the graph nodes"
-        my_labeler = Labeler ("../domain/"+domain,
+        my_labeler = Labeler ("../domain/"+domain+"/"+task,
                               unique_name+"basic_graph",
                               unique_name,
                               unique_name+"id_to_phrase.pkl",
                               unique_name+"phrase_to_id.pkl",
                               max_number_of_labels)
-        my_labeler.add_labels_to_graph(label_type)
+        if ppdb_type == "lexical":
+            my_labeler.add_labels_to_graph(label_mode)
+        elif ppdb_type == "phrasal":
+            my_labeler.add_labels_to_graph(2)
+        
         print "Start of pruning graph"
-        my_labeler.prune_graph(unique_name+"basic_graph", unique_name+"seeds", "../domain/europarl/oov_list", graph_type, "knn", knn)
+        my_labeler.prune_graph(unique_name+"basic_graph", unique_name+"seeds", "../domain/"+domain+"/"+task+"/"+"oov_list", graph_type, "knn", knn)
         print "End of pruning graph"
         my_labeler.save_to_file()
         print "transfering label done!"
         print "Now it is ready for propagation"
 
     else:
-        print "You need to pass size, type, and domain to this program"
-        print "Something like python command.py xl lexical europarl"
+        print "You need to pass size, type, language, and domain to this program"
+        print "Something like python command.py xl lexical en europarl"
--- ppdb/grammar_appending.py
+++ ppdb/grammar_appending.py
@@ -1,7 +1,21 @@
 import gzip
+import sys
 import pickle
 from label_extracter import Extractor
 class grammarGenerator:
+
+    def get_phrases(self, tokens):
+        return self.ngrams(tokens, 1, 7)
+
+
+    def ngrams(self, tokens, MIN_N, MAX_N):
+        result = []
+        n_tokens = len(tokens)
+        for i in xrange(n_tokens):
+            for j in xrange(i+MIN_N, min(n_tokens, i+MAX_N)+1):
+                result.append(" ".join(tokens[i:j]))
+        return result
+
     def __init__(self, extractor, grammar_files_path, grammar_output_files_path, text_file, oov_list_file):
         self.extractor = extractor
         self.text_file = text_file
@@ -11,6 +25,53 @@ class grammarGenerator:
         with open(oov_list_file) as inp:
             for line in inp:
                 self.oov_list.append(line.strip())
+
+
+    def unseen_phrases(self,GSCFG=True):
+        with open(self.text_file) as inp:
+            i = 0
+            for sent in inp:
+                grammar_file = gzip.open(self.grammar_files_path+"/grammar."+str(i)+".gz")
+                new_grammar_file = gzip.open(self.grammar_output_files_path+"/grammar."+str(i)+".gz",'w')
+                for line in grammar_file:
+                    parts = line.strip().split(" ||| ")
+                    rule = parts[0:4]
+                    alignment = parts[4]
+                #srule = " ||| ".join(rule)
+                #print rule
+                #temp = parts[3]
+                #rem = parts[3:]
+                    rule = " ||| ".join(rule) + " Seed=1.0" + " ||| "+ alignment
+                    new_grammar_file.write(rule+"\n")
+                    #print rule + "\n"
+                # TODO phrase
+                phrases = self.get_phrases(sent.split(" ||| ")[0].split())
+
+                #TODO Named Entity Recognizer
+                #print sent
+                if GSCFG == True:
+                    for phrase in phrases:
+                        #print phrase
+                        if phrase in self.oov_list:
+                            #print word
+                            if self.extractor.contains(phrase):
+                                #print >> sys.stderr, '%s as an oov phrase found in graph'%phrase
+                                label_list = self.extractor.get_labels(phrase)
+                                for (rule,p) in label_list:
+                                    prob = float(p)
+                                    lexf = str(float(len(phrase)))
+                                    lexe = str(float(len(rule.split(" ||| ")[2])))
+                                    srule = rule.replace('~~~',phrase, 1)+" ||| EgivenFCoherent=2.47712135315 SampleCountF=2.47856640816 CountEF=1.0 MaxLexFgivenE="+lexf+" MaxLexEgivenF="+lexe+" IsSingletonF=0.0 IsSingletonFE=0.0 newF=1.0 "+"Seed="+ str(prob)
+
+
+                                    srule = rule.replace('~~~',phrase, 1)+" ||| EgivenFCoherent=1.0 SampleCountF=1.0 CountEF=1.0 MaxLexFgivenE=1.0 MaxLexEgivenF=1.0 IsSingletonF=1.0 IsSingletonFE=1.0 newF=1.0 "+"Seed="+ str(prob)
+                                    new_grammar_file.write(srule+"\n")
+                                    #print srule + "\n"
+                grammar_file.close()
+                new_grammar_file.close()
+                i += 1
+
+
     def just_oovs(self,GSCFG=True):
         with open(self.text_file) as inp:
             i = 0
@@ -37,15 +98,17 @@ class grammarGenerator:
                     for word in words:
                         #print word
                         if word in self.oov_list:
-                            print word
+                            #print word
                             if self.extractor.contains(word):
-                                print word
+                                #print >> sys.stderr, '%s as an oov phrase found in graph'
                                 label_list = self.extractor.get_labels(word)
                                 for (rule,p) in label_list:
                                     prob = float(p)
-                                    srule = rule.replace('~~~',word, 1)+" ||| EgivenFCoherent=1.0 SampleCountF=1.0 CountEF=1.0 MaxLexFgivenE=1.0 MaxLexEgivenF=1.0 IsSingletonF=1.0 IsSingletonFE=1.0 newF=0.0 "+"Seed="+ str(prob)
+                                    lexf = str(float(len(word)))
+                                    lexe = str(float(len(rule.split(" ||| ")[2])))
+                                    srule = rule.replace('~~~',word, 1)+" ||| EgivenFCoherent=2.47712135315 SampleCountF=2.47856640816 CountEF=1.0 MaxLexFgivenE="+lexf+" MaxLexEgivenF=1.0 IsSingletonF=1.0 IsSingletonFE=1.0 newF=1.0 "+"Seed="+ str(prob)
                                     new_grammar_file.write(srule+"\n")
-                                    print srule + "\n"
+                                    #print srule + "\n"
                 grammar_file.close()
                 new_grammar_file.close()
                 i += 1
@@ -54,14 +117,49 @@ class grammarGenerator:
 
 
 if __name__=="__main__":
+
+
+    # for oovs use oov_list , for other use phrase file like dev.phrases
+
+    uniq_prefix = "europarl_en-es_m_phrasal"
+    MT_prefix = "../domain/europarl/en-es"
+    propagation_prefix = "experiments/europarl_en-es_m_phrasal"
+
     testExtractor = Extractor()
-    testExtractor.extract(phrase_list_file="../domain/europarl/oov_list", propagation_output="pruned_second_test", id_to_phrase_file="europarl_xl_lexical_id_to_phrase.pkl",id_to_label_file="europarl_xl_lexical_id_to_label.pkl", max_number_of_labels=100)
+    testExtractor.extract(phrase_list_file=MT_prefix+"/dev.phrases", propagation_output=propagation_prefix+"_finalitr1", id_to_phrase_file=propagation_prefix+"_id_to_phrase.pkl",id_to_label_file=propagation_prefix+"_id_to_label.pkl", max_number_of_labels=100)
+#      
+    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path=MT_prefix+"/old/dev.grammars", grammar_output_files_path=MT_prefix+"/europarl_en-es_m_phrasal_itr1_newfeatures/dev.grammars", text_file=MT_prefix+"/dev.en-es", oov_list_file=MT_prefix+"/dev.phrases")
+#
+    testGrammarGenerator.unseen_phrases(GSCFG =True)
+#
+    testExtractor.extract(phrase_list_file=MT_prefix+"/devtest.phrases", propagation_output=propagation_prefix+"_finalitr1", id_to_phrase_file=propagation_prefix+"_id_to_phrase.pkl",id_to_label_file=propagation_prefix+"_id_to_label.pkl", max_number_of_labels=100)
+#
+#
+    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path=MT_prefix+"/old/devtest.grammars", grammar_output_files_path=MT_prefix+"/europarl_en-es_m_phrasal_itr1_newfeatures/devtest.grammars", text_file=MT_prefix+"/devtest.en-es", oov_list_file=MT_prefix+"/devtest.phrases")
+#
+    testGrammarGenerator.unseen_phrases(GSCFG =True)
+
+
+    ## europarl_en-es_l_phrasal_label_dic.pkl , europarl_en-es_l_phrasal_graph_dic.pkl 
+
+#    uniq_prefix = "europarl_en-es_l_phrasal"
+#    MT_prefix = "../domain/europarl/en-es"
+#    propagation_prefix = "madll_experiments/"
+#    itr_number = "2"
+
+#    testExtractor = Extractor()
+#    testExtractor.extract(phrase_list_file=MT_prefix+"/dev.phrases", propagation_output=propagation_prefix+uniq_prefix+"_itr"+itr_number, id_to_phrase_file=propagation_prefix+uniq_prefix+"_id_to_phrase.pkl",id_to_label_file=propagation_prefix+uniq_prefix+"_id_to_label.pkl", max_number_of_labels=20)
+        
+#    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path=MT_prefix+"/old/dev.grammars", grammar_output_files_path=MT_prefix+"/"+uniq_prefix+"_itr"+itr_number+"_madll"+"/dev.grammars", text_file=MT_prefix+"/dev.en-es", oov_list_file=MT_prefix+"/dev.phrases")
       
-    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path="../domain/europarl/old/dev.grammars", grammar_output_files_path="../domain/europarl/new_xl_itr9_pr_oov/dev.grammars", text_file="../domain/europarl/dev.en-es", oov_list_file="../domain/europarl/oov_list")
+#    testGrammarGenerator.unseen_phrases(GSCFG =True)
 
-    testGrammarGenerator.just_oovs(GSCFG =True)
 
-    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path="../domain/europarl/old/devtest.grammars", grammar_output_files_path="../domain/europarl/new_xl_itr9_pr_oov/devtest.grammars", text_file="../domain/europarl/devtest.en-es", oov_list_file="../domain/europarl/oov_list")
 
-    testGrammarGenerator.just_oovs(GSCFG =True)
 
+#    testExtractor = Extractor()
+#    testExtractor.extract(phrase_list_file=MT_prefix+"/devtest.phrases", propagation_output=propagation_prefix+uniq_prefix+"_itr"+itr_number, id_to_phrase_file=propagation_prefix+uniq_prefix+"_id_to_phrase.pkl",id_to_label_file=propagation_prefix+uniq_prefix+"_id_to_label.pkl", max_number_of_labels=20)
+        
+#    testGrammarGenerator = grammarGenerator(extractor=testExtractor, grammar_files_path=MT_prefix+"/old/devtest.grammars", grammar_output_files_path=MT_prefix+"/"+uniq_prefix+"_itr"+itr_number+"_madll"+"/devtest.grammars", text_file=MT_prefix+"/devtest.en-es", oov_list_file=MT_prefix+"/devtest.phrases")
+      
+#    testGrammarGenerator.unseen_phrases(GSCFG =True)
